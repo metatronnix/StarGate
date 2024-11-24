e952d5c62534529615a6f1596ad7b2f703abf846
Richard Gay <rich5673@hotmail.com>
2024-11-24 20:53:28 +0000

    Star-11:   1. Remove references to EntityFrameworkCore.Sqlite   2. Add references to EntityFrameWorkCore.SqlServer  3. Fix affected files.

--- StarGateAPI/Program.cs
+++ StarGateAPI/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StargateContext>(options => 
-    options.UseSqlite(builder.Configuration.GetConnectionString("StarbaseApiDatabase")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StarbaseApiDatabase")));
 
 builder.Services.AddMediatR(cfg =>
 {
