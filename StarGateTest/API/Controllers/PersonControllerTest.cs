07236f33760b74bf081d9cf64a77024b2f9d4a55
Administrator <Administrator@EC2AMAZ-PF5VR85>
2024-11-24 05:19:28 +0000

    Add project files.

---
+++ StarGateTest/API/Controllers/PersonControllerTest.cs
@@ -0,0 +1,15 @@
+﻿namespace StarGateTest;
+
+public class PersonControllerTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ StarGateTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace StarGateTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
