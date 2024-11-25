4c521020b1aa5c7c22d8b3c475770fdc23567aa6
Richard Gay <rich5673@hotmail.com>
2024-11-25 12:05:03 +0000

    STAR-44

---
+++ StarGateAPI/Business/Dtos/BaseCloudRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace StargateAPI.Business.Dtos
+{
+    public class BaseCloudRequest
+    {
+        public string? KeyID { get; set; }
+        public string? SecretKeyId { get; set; }
+        public string? CredentialsLabel { get; set; }
+    }
+}
---
+++ StarGateAPI/Business/Dtos/SecretResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace StargateAPI.Business.Dtos
+{
+    public class SecretResponse
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string Engine { get; set; } = string.Empty;
+
+        public string Host { get; set; } = string.Empty;
+
+        public int Port { get; set; }
+
+        public string DBInstanceIdentifier { get; set; } = string.Empty;
+    }
+}
